d217ca3ffd5d53de7f950617cc6d8b5056a0eb6f
Andrew Collins <andycollins35@gmail.com>
2017-08-11 11:09:19 +0100

    Update ConfigExplorerMiddleware.cs

--- src/ConfigExplorerMiddleware.cs
+++ src/ConfigExplorerMiddleware.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
