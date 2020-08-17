744456aa04bea48cec34a856e6eca79222edd955
Chad Tolkien <chad@sodadigital.com.au>
2020-08-17 22:08:44 +1000

    prep for 3.0 release

--- src/ConfigExplorerOptions.cs
+++ src/ConfigExplorerOptions.cs
@@ -2,8 +2,19 @@
 {
     public class ConfigExplorerOptions
     {
+        /// <summary>
+        /// The path to expose this information on. Defaults to "/config"
+        /// </summary>
         public string PathMatch { get; set; } = "/config";
+
+        /// <summary>
+        /// Will only run when on localhost. Defaults to true
+        /// </summary>
         public bool LocalHostOnly { get; set; } = true;
+
+        /// <summary>
+        /// Will attempt to redact configuration that looks like connection strings. Defaults to true
+        /// </summary>
         public bool TryRedactConnectionStrings { get; set; } = true;
     }
 }
